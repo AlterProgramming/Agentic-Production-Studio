162956e335cbe16eae3d2f05b1d4c4f4870dd221
AlterProgramming <alterprogramming@gmail.com>
2026-08-02 23:25:56 -0500

    Export video project runtime

--- studio_runtime/__init__.py
+++ studio_runtime/__init__.py
@@ -3,13 +3,27 @@
 from .events import EventBuffer, IntentEvent
 from .recreation import RatePolicy, RecreationConfig, RecreationPipeline
 from .server import StudioService, create_server
+from .video import (
+    CommandVideoEvaluator,
+    CommandVideoGenerator,
+    ShotSpec,
+    VideoProjectConfig,
+    VideoProjectPipeline,
+    VideoRatePolicy,
+)
 
 __all__ = [
+    "CommandVideoEvaluator",
+    "CommandVideoGenerator",
     "EventBuffer",
     "IntentEvent",
     "RatePolicy",
     "RecreationConfig",
     "RecreationPipeline",
+    "ShotSpec",
     "StudioService",
+    "VideoProjectConfig",
+    "VideoProjectPipeline",
+    "VideoRatePolicy",
     "create_server",
 ]
