6eac1e2373038c94c9c62215949e8371d384e9b7
AlterProgramming <alterprogramming@gmail.com>
2026-08-02 22:18:39 -0500

    Export recreation runtime API

--- studio_runtime/__init__.py
+++ studio_runtime/__init__.py
@@ -1,6 +1,15 @@
 """Live intent-stream surface for Production Studio runtimes."""
 
 from .events import EventBuffer, IntentEvent
+from .recreation import RatePolicy, RecreationConfig, RecreationPipeline
 from .server import StudioService, create_server
 
-__all__ = ["EventBuffer", "IntentEvent", "StudioService", "create_server"]
+__all__ = [
+    "EventBuffer",
+    "IntentEvent",
+    "RatePolicy",
+    "RecreationConfig",
+    "RecreationPipeline",
+    "StudioService",
+    "create_server",
+]
